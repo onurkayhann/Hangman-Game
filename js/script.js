e0d669c2b282158097e3e9a94e9bfa48adb304d9
Onur Kayhan <onur.kayhan@chasacademy.se>
2021-11-23 16:23:54 +0100

    updateded wordList and modified selectedWord

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
 // Globala variabler
 
-const wordList;      // Array: med spelets alla ord
-let selectedWord;    // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+const wordList = [
+  "Lemonad",
+  "Thaiboxning",
+  "Barcelona",
+  "Spanska",
+  "Fullstack",
+];
+let selectedWord = wordList[Math.floor(Math.random() * wordList.length)]; // Sträng: ett av orden valt av en slumpgenerator från arrayen ovan
+console.log(selectedWord);
 
-let guesses = 0;     // Number: håller antalet gissningar som gjorts
-let hangmanImg;      // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
+let guesses = 0; // Number: håller antalet gissningar som gjorts
+let hangmanImg; // Sträng: sökväg till bild som kommer visas (och ändras) fel svar. t.ex. `/images/h1.png`
 
-let msgHolderEl;     // DOM-nod: Ger meddelande när spelet är över
-let startGameBtnEl;  // DOM-nod: knappen som du startar spelet med
+let msgHolderEl; // DOM-nod: Ger meddelande när spelet är över
+let startGameBtnEl; // DOM-nod: knappen som du startar spelet med
 let letterButtonEls; // Array av DOM-noder: Knapparna för bokstäverna
-let letterBoxEls;    // Array av DOM-noder: Rutorna där bokstäverna ska stå
+let letterBoxEls; // Array av DOM-noder: Rutorna där bokstäverna ska stå
 
 // Funktion som startar spelet vid knapptryckning, och då tillkallas andra funktioner
 // Funktion som slumpar fram ett ord
